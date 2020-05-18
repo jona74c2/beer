cf34c1b8450236de790fd298159960a67c4e0a6c
Jonas Friis Kjær <jona74c2@stud.kea.dk>
2020-05-18 08:57:54 +0200

    dashboard now knows how many beers of each kind has been ordered

--- dashboard.js
+++ dashboard.js
@@ -2,26 +2,124 @@
 
 window.addEventListener("DOMContentLoaded", start);
 
-let jsonData;
 const settings = {};
-const logData = [];
+const beerCount = {};
 
 function start() {
+  settings.firstBuild = true;
   settings.interval = 2000;
-
   timerFunction();
 }
 
 async function getData() {
   const url = "https://holbech-bestbrewer.herokuapp.com/";
   const response = await fetch(url);
-  jsonData = await response.json();
-  console.table(jsonData);
+  const jsonData = await response.json();
+
+  console.log(jsonData);
+  return jsonData;
 }
 
-function timerFunction() {
-  getData();
+async function timerFunction() {
+  let orderData = await getData();
+  if (Object.keys(orderData).length > 0 && orderData.constructor === Object) {
+    builder(orderData);
+  }
+
   setTimeout(timerFunction, settings.interval);
+}
+
+function builder(orderData) {
+  if (orderData === undefined) {
+    console.log("Return return");
+    return;
+  }
+  let orderDataArray = [];
+  if (settings.firstBuild) {
+    initBeerCount(orderData);
+    orderDataArray = initOrderData(orderData);
+    settings.firstBuild = false;
+  } else {
+    orderDataArray = updateOrderData(orderData);
+    console.log("orderDataArray: ", orderDataArray);
+  }
+  if (orderDataArray.length > 0) {
+    countBeer(orderDataArray);
+    console.log("We are in!");
+  } else {
+    console.log("no updates");
+  }
+}
+
+function updateOrderData(orderData) {
+  let subArray = [];
+  if (orderData.serving === undefined) {
+    return subArray;
+  }
+
+  console.log("lastArrayItem: ", beerCount.lastArrayItem);
+  beerCount.foundLastArrayItem = false;
+
+  subArray = addNewOrders(orderData.serving).concat(addNewOrders(orderData.queue));
+  /* let temp = addNewOrders(orderData.queue);
+  subArray = subArray.concat(temp); */
+  if (orderData.serving === undefined) {
+    beerCount.lastArrayItem = orderData.serving[orderData.serving.length - 1];
+  } else {
+    beerCount.lastArrayItem = orderData.queue[orderData.queue.length - 1];
+  }
+
+  if (!beerCount.foundLastArrayItem) {
+    return orderData.serving.concat(orderData.queue);
+  }
+  return subArray;
+}
+
+function addNewOrders(array) {
+  let subArray2 = [];
+  array.forEach((order) => {
+    console.log("order: ", order);
+    if (beerCount.foundLastArrayItem) {
+      subArray2.push(order);
+      console.log("item pushed pushed");
+    } else if (beerCount.lastArrayItem !== undefined && order.id === beerCount.lastArrayItem.id) {
+      beerCount.foundLastArrayItem = true;
+      console.log("item found!", order.id, " ", beerCount.lastArrayItem.id);
+    }
+  });
+  return subArray2;
+}
+
+function initOrderData(orderData) {
+  console.log(orderData);
+  orderData = orderData.serving.concat(orderData.queue);
+  if (orderData.queue !== undefined) {
+    beerCount.lastArrayItem = orderData.queue[orderData.queue.length - 1];
+  }
+  return orderData;
+}
+
+function initBeerCount(orderData) {
+  orderData.taps.forEach((tap, index) => {
+    beerCount["beerTap" + index] = tap.beer;
+    beerCount["ordersTap" + index] = 0;
+  });
+  console.group(beerCount);
+}
+
+function countBeer(orderData) {
+  console.log(orderData);
+  // orderData
+  orderData.forEach((order) => {
+    order.order.forEach((beer) => {
+      for (let i = 0; i < 7; i++) {
+        if (beerCount["beerTap" + i] === beer) {
+          beerCount["ordersTap" + i]++;
+        }
+      }
+    });
+  });
+  console.log(beerCount);
 }
 
 /* function setLogData() {
